476c75004b1e0f2ed685384569aadf03fb77b853
AshokSubramanium98 <ashok@dev-bee.com>
2021-07-03 10:36:56 +0530

    #Add home section

--- src/Components/LandingPage/index.js
+++ src/Components/LandingPage/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import {OfferBar} from '../Ui/Offer';
 import Header from '../Ui/Header';
+import Home from './home';
 import Footer from '../Ui/Footer';
 
 const LandingPage = () => (
     <div>
         <OfferBar />
         <Header />
+        <Home />
         <Footer />
     </div>
 );
