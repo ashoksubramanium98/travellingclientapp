7971257b9547c3014c20fbb8c608e35dc6c0c3ed
AshokSubramanium98 <ashok@dev-bee.com>
2021-07-03 10:36:46 +0530

    #Add button in ui

---
+++ src/Components/Ui/Button/index.js
@@ -0,0 +1,21 @@
+import styled from "styled-components";
+
+export const Button = styled.button`
+    width: 160px;
+    color: #fff;
+    font-size: 18px;
+    font-weight: 700;
+    border: none;
+    border-radius: 5px;
+    padding: 12px;
+    cursor: pointer;
+    background: linear-gradient(90deg, hsla(0, 0%, 0%, 1) 0%, hsla(212, 64%, 21%, 1) 100%);
+    :hover{
+        opacity: 0.98;
+    }
+    @media(max-width: 600px){
+        width: 140px;
+        font-weight: 600;
+        font-size: 16px;
+    }
+`;
