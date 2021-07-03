7fe838efc6b4b81f19f21f79ef2cce023494e3a4
AshokSubramanium98 <ashok@dev-bee.com>
2021-07-03 23:46:51 +0530

    #Fix time for offerBar

--- src/Components/Ui/Offer/index.js
+++ src/Components/Ui/Offer/index.js
@@ -13,8 +13,7 @@ const TimerOptionsDiv = styled.span`
 `;
 
 export const OfferBar = () => {
-    const STARTING_TODAY = 3, ENDING_TODAY = 7;
-    let endDate = new Date(); endDate.setDate(STARTING_TODAY + ENDING_TODAY); endDate.setHours(23); endDate.setMinutes(59); endDate.setSeconds(59); endDate = Math.floor(endDate/1000);
+    let endDate = new Date(); endDate.setDate(10); endDate.setHours(17); endDate.setMinutes(59); endDate.setSeconds(59); endDate = Math.floor(endDate/1000);
 
     const CURRENT_DATE = Date.now();
     const offerDuration = (endDate*1000 - CURRENT_DATE);
